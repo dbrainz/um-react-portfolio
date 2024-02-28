7f3917357c8b65a5ffa2ed60d6e2f79b41892bb4
Darren Brain <darren.brain@gmail.com>
2024-02-28 11:27:38 -0500

    Add links and images to footer

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ export default function About() {
     return (
         <div>
             <p>
-                About Darren
+                Years ago, I used to code for a living. Then life came along, and things hapoened and I had to step away from that. But, things have changed again and I'd like to do it professionally again as it was the only profession I truly enjoyed.
             </p>
         </div>
     )
