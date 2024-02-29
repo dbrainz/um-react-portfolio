cb1139265f4ea2b8a870b6cff2a4cf0dd670664b
Darren Brain <darren.brain@gmail.com>
2024-02-29 00:24:50 -0500

    Add profile pic

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,7 @@
 export default function About() {
     return (
-        <div>
+        <div> 
+            <img id="profile-pic" src="./assets/images/heisenbrain297x297-transparent.png" alt="Profile pic" />
             <p>
                 Years ago, I used to code for a living. Then life came along, and things hapoened and I had to step away from that. But, things have changed again and I'd like to do it professionally again as it was the only profession I truly enjoyed.
             </p>
